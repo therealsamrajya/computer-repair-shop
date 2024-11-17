89195f7aabf1f22dfe77e015a2fe030286e0d217
therealsamrajya <samrajyadai@gmail.com>
2024-11-17 13:11:58 +0545

    try

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,20 +1,17 @@
 import { db } from "./index";
-import {migrate} from "drizzle-orm/neon-http/migrator"
+import { migrate } from "drizzle-orm/neon-http/migrator";
 
-const main = async ()=>{
-    //migration is done to change the db if any changes are need later on
-    try{
-        await migrate(db,{
-            migrationsFolder:'src/db/migrations'
-        })
-        console.log("Migration Complete!!");
-        
-    }
-    catch(error){
-        console.error("An error occcured :",error);
-        process.exit(1)
-    }
-}
+const main = async () => {
+  //migration is done to change the db if any changes are need later on
+  try {
+    await migrate(db, {
+      migrationsFolder: "src/db/migrations",
+    });
+    console.log("Migration Complete!!");
+  } catch (error) {
+    console.error("An error occcured :", error);
+    process.exit(1);
+  }
+};
 
-
-main()
+main();
